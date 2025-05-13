65b86722ca31689655607465cf6f2a1bcfce747a
Maël <119709626+maeldolium@users.noreply.github.com>
2025-05-13 08:33:50 +0200

    Modify favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/Logo.png" />
+        <link rel="icon" href="/Logo_1.png" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
